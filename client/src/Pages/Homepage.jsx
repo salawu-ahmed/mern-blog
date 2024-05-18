0fc15217c48e4b488c082d076155a072e25575c0
ahmed <a.salawu@outlook.com>
2024-05-18 07:13:51 +0000

    Created Homepage, Loginpage & Registerpage

---
+++ client/src/Pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from '../Post'
+
+const Homepage = () => {
+  return (
+    <>
+      <Post/>
+      <Post/>
+      <Post/>
+    </>
+  )
+}
+
+export default Homepage
---
+++ client/src/Pages/Loginpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loginpage = () => {
+  return (
+    <form>
+        <h1>Login</h1>
+        <input type="text" placeholder='username' />
+        <input type="password" placeholder='password' />
+        <button>Login</button>
+    </form>
+  )
+}
+
+export default Loginpage
