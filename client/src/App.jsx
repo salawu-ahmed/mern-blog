5e7835b9881159abc189b772544c68a948081f59
ahmed <a.salawu@outlook.com>
2024-05-18 07:17:01 +0000

    configured react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,20 @@
 import './App.css'
+import Layout from './Layout'
+import Homepage from './Pages/Homepage'
+import Loginpage from './Pages/Loginpage'
+import { Route, Routes } from 'react-router-dom'
+import Registerpage from './Pages/Register'
 
 function App() {
 
   return (
-    <main>
-      <header>
-        <a href="#" className='logo'>My Blog</a>
-        <nav><a href="">Login</a><a href="">Register</a></nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="../src/assets/pexels-binyaminmellish-106399.jpg" alt="" />
-        </div>
-        <div className="texts">
-          <h2 className="title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
-          <p className="info">
-            <a href="#" className="author">Salawu Ahmed</a>
-            <time datetime="">23-01-06 16:45</time>
-          </p>
-          <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, aperiam quis perspiciatis harum explicabo laborum earum assumenda! Dolorem error exercitationem aut dolor architecto, recusandae iste quaerat ipsum nemo. Autem, odio!</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="../src/assets/pexels-binyaminmellish-106399.jpg" alt="" />
-        </div>
-        <div className="texts">
-          <h2 className="title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
-          <p className="info">
-            <a href="#" className="author">Salawu Ahmed</a>
-            <time datetime="">23-01-06 16:45</time>
-          </p>
-          <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, aperiam quis perspiciatis harum explicabo laborum earum assumenda! Dolorem error exercitationem aut dolor architecto, recusandae iste quaerat ipsum nemo. Autem, odio!</p>
-        </div>
-      </div>
-        <div className="post">
-          <div className="image">
-            <img src="../src/assets/pexels-binyaminmellish-106399.jpg" alt="" />
-          </div>
-          <div className="texts">
-            <h2 className="title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
-            <p className="info">
-              <a href="#" className="author">Salawu Ahmed</a>
-              <time datetime="">23-01-06 16:45</time>
-            </p>
-            <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, aperiam quis perspiciatis harum explicabo laborum earum assumenda! Dolorem error exercitationem aut dolor architecto, recusandae iste quaerat ipsum nemo. Autem, odio!</p>
-          </div>
-        </div>
-
-      
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Homepage/>}/>
+        <Route path="/login" element={<Loginpage/>}/>
+        <Route path="/register" element={<Registerpage/>}/>
+      </Route>
+    </Routes>
   )
 }
 
