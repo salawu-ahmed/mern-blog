e9dc0905c7d3f865a94cfa200a612a4c6b5b516d
ahmed <a.salawu@outlook.com>
2024-05-20 01:03:43 +0000

    created register fetch logic

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const register = async (e) => {
+        e.preventDefault()
+        try {
+            await fetch('http://localhost:4000/register',{
+                method: "POST",
+                body: JSON.stringify({
+                    username,
+                    password
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }})
+            } catch (error) {
+                console.error(error);
+            }
+        }
   return (
-    <form>
+    <form onSubmit={register}>
         <h1>Register</h1>
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
+        <input type="text" placeholder='username' onChange={(e) => setUsername(e.target.value)} value={username}/>
+        <input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)} value={password}/>
         <button>Register</button>
     </form>
   )
