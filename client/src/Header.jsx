8cddbc5cb2c76eda0088eaa0e347fe213f569c4f
ahmed <a.salawu@outlook.com>
2024-05-20 17:51:33 +0000

    created dynamic header based on user login

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const [username, setUsername] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:4000/profileInfo', {
+            credentials: "include"
+        })
+            .then((res) => res.json())
+            .then(data => setUsername(data.username))
+    }, [])
     return (
         <header>
             <Link to="/" className='logo'>My Blog</Link>
+            <Link to='/logout'>log out</Link>
             <nav>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+                {
+                    username && (
+                        <>
+                            <Link to={'/create'}>Create post</Link>
+                        </>
+                    )
+                }
+                {
+                    !username && (
+                        <>
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </>
+                    )
+                }
             </nav>
         </header>
     )
