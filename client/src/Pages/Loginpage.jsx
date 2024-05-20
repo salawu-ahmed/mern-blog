95e4bf9ec325926ff43838434eaa1424d49fa20f
ahmed <a.salawu@outlook.com>
2024-05-20 17:06:32 +0000

    sent login credentials to backend successfully

--- client/src/Pages/Loginpage.jsx
+++ client/src/Pages/Loginpage.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Loginpage = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const login = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await fetch('http://localhost:4000/login', {
+        method: "POST",
+        body: JSON.stringify({username, password}),
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: 'include'
+      })
+      if(res.ok) {
+        alert("Login successful")
+      }
+    } catch (error) {
+      console.log(error)
+      alert
+    }
+
+  }
+
   return (
-    <form>
+    <form className='login' onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
+        <input type="text" placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
         <button>Login</button>
     </form>
   )
