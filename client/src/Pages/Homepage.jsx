b4adfe653a0dd1614221433554218f64fb0f3c4f
ahmed <a.salawu@outlook.com>
2024-05-21 12:30:18 +0000

    fetching and displaying posts from database

--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from '../Post'
 
 const Homepage = () => {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:4000/posts')
+    .then((res) => res.json())
+    .then((posts) => setPosts(posts))
+  }, [])
   return (
     <>
-      <Post/>
-      <Post/>
-      <Post/>
+    {
+      posts.length > 0 && (
+        posts.map((post) => <Post {...post} key={post._id}/> )
+      )
+    }
     </>
   )
 }
--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import {format} from 'date-fns'
 
-const Post = () => {
+const Post = ({title, summary, content, cover, createdAt}) => {
     return (
         <div className="post">
             <div className="image">
                 <img src="../src/assets/pexels-binyaminmellish-106399.jpg" alt="" />
             </div>
             <div className="texts">
-                <h2 className="title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+                <h2 className="title">{title}</h2>
                 <p className="info">
                     <a href="#" className="author">Salawu Ahmed</a>
-                    <time dateTime="">23-01-06 16:45</time>
+                    <time dateTime="">{format(new Date(createdAt), "yyyy-MM-dd")}</time>
                 </p>
-                <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, aperiam quis perspiciatis harum explicabo laborum earum assumenda! Dolorem error exercitationem aut dolor architecto, recusandae iste quaerat ipsum nemo. Autem, odio!</p>
+                <p className='summary'>{summary}</p>
             </div>
         </div>
     )
