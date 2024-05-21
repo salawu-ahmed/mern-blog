202bb7d5f157093377d916188681c76afbfa05e3
ahmed <a.salawu@outlook.com>
2024-05-21 13:56:19 +0000

    read post details from database

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {format} from 'date-fns'
 
-const Post = ({title, summary, content, cover, createdAt}) => {
+const Post = ({title, summary, content, cover, createdAt, author}) => {
     return (
         <div className="post">
             <div className="image">
-                <img src="../src/assets/pexels-binyaminmellish-106399.jpg" alt="" />
+                <img src={'http://localhost:4000/'+cover} alt="" />
             </div>
             <div className="texts">
                 <h2 className="title">{title}</h2>
                 <p className="info">
-                    <a href="#" className="author">Salawu Ahmed</a>
+                    <a href="#" className="author">{author.username}</a>
                     <time dateTime="">{format(new Date(createdAt), "yyyy-MM-dd")}</time>
                 </p>
                 <p className='summary'>{summary}</p>
