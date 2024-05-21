bc88fde9c09526f5707d96aa648eab7493ce6b63
ahmed <a.salawu@outlook.com>
2024-05-21 11:23:20 +0000

    sent post form data to server

--- client/src/Pages/Createpost.jsx
+++ client/src/Pages/Createpost.jsx
@@ -1,17 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 
+const modules = {
+    toolbar: [
+        [{ font: [] }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ color: [] }, { background: [] }],
+        [{ script: "sub" }, { script: "super" }],
+        ["blockquote", "code-block"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+        ["link", "image", "video"],
+        ["clean"],
+    ]
+}
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+]
+
 const Createpost = () => {
-  return (
-    <form>
-      <input type='text' placeholder='Title'/>
-      <input type="summary" placeholder='summary' />
-      <input type="file" name="" id="" />
-      <ReactQuill />
-      <button style={{marginTop: '5px'}}>Create post</button>
-    </form>
-  )
+    const [title, setTitle] = useState('')
+    const [summary, setSummary] = useState('')
+    const [content, setContent] = useState('')
+    const [files, setFiles] = useState('')
+    const data = new FormData()
+    data.set('title', title)
+    data.set('summary', summary)
+    data.set('content', content)
+    data.set('file', files[0])
+    const createNewPost = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await fetch('http://localhost:4000/createpost', {
+                method: 'POST',
+                body: data
+            })
+            const received = await res.json()
+            console.log(received);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <form onSubmit={createNewPost}>
+            <input
+                type='text'
+                placeholder='Title'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+                type="summary"
+                placeholder='summary'
+                value={summary}
+                onChange={(e) => setSummary(e.target.value)}
+            />
+            <input
+                type="file"
+                name="file"
+                id=""
+                onChange={(e) => setFiles(e.target.files)}
+            />
+            <ReactQuill
+                value={content}
+                modules={modules}
+                formats={formats}
+                onChange={newValue => setContent(newValue)}
+            />
+            <button
+                style={{ marginTop: '5px' }}
+            >
+                Create post
+            </button>
+        </form>
+    )
 }
 
 export default Createpost
