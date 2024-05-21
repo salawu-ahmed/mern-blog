c01415a1092eb9f14714a4163db5e370a03970fd
ahmed <a.salawu@outlook.com>
2024-05-21 01:33:54 +0000

    provided usercontext to entire app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,20 @@ import Homepage from './Pages/Homepage'
 import Loginpage from './Pages/Loginpage'
 import { Route, Routes } from 'react-router-dom'
 import Registerpage from './Pages/Register'
+import UserContextProvider from './UserContextProvider'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Homepage/>}/>
-        <Route path="/login" element={<Loginpage/>}/>
-        <Route path="/register" element={<Registerpage/>}/>
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/register" element={<Registerpage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   )
 }
 
